36d6e1e9ce149f2cda0f9332e35255c3afcef2b1
Woo Sung Jahng <jlittle0919@icloud.com>
2024-11-30 17:55:51 +0900

    Edited imports on database.py

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,15 @@
-import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials, db, initialize_app
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+
 
 BASE_DIR = Path(__file__).parent.parent
 load_dotenv(BASE_DIR / '.env')
 
 try:
     cred = credentials.Certificate(BASE_DIR / './serviceAccountKey.json')
-    firebase_admin.initialize_app(cred, {
+    initialize_app(cred, {
         'databaseURL': os.getenv('FIREBASE_DATABASE_URL')
     })
     posts_ref = db.reference('posts')
