0049bc236114409007532fec622a9a7eb92549b5
Woo Sung Jahng <jlittle0919@icloud.com>
2024-12-01 13:10:27 +0900

    Add categories for posts

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from database import posts_ref
 from pydantic import BaseModel
+from enum import Enum
+from typing import List
 
 
 app = FastAPI()
@@ -13,15 +15,24 @@ app.mount("/static", StaticFiles(directory="../frontend"), name="static")
 async def read_index():
     return FileResponse("../frontend/index.html")
 
+class CategoryEnum(str, Enum):
+    ETC = "etc",
+    STUDY = "study",
+    WORK = "work",
+    RELATIONSHIP = "relationship",
+    LOVE = "love"
+
 class Post(BaseModel):
     title: str
     body: str
+    categories: List[CategoryEnum]
 
 @app.post("/posts")
 async def create_post(post: Post):
     new_post = {
         "title": post.title,
         "body": post.body,
+        "categories": [cat.value for cat in post.categories],
         "timestamp": {'.sv': 'timestamp'}
     }
     posts_ref.push(new_post)
